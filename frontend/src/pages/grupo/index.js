fea47ccf234853017f32ec1118341bd336392462
Luiz Carlos <d71k1n@live.com>
2020-07-14 13:23:59 -0300

    updated index

--- frontend/src/pages/grupo/index.js
+++ frontend/src/pages/grupo/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Button,
   Container,
@@ -12,62 +12,53 @@ import {
 } from "rbx";
 import MenuSuperior from "../../components/navbar";
 
-export default class Grupo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      teste: ""
-    };
+export default function Grupo() {
+  const [teste, setTeste] = useState("");
+
+  function mensagem() {
+    alert(teste);
   }
 
-  //  - funcoes aqui
-  gravarGrupoMuscular = () => {
-    alert(this.state.teste);
-  };
-
-  render() {
-    return (
-      <div>
-        <MenuSuperior />
-        <Container>
-          <Column.Group centered>
-            <Column>
-              <Title size={3}>Cadastro de Grupo muscular</Title>
-            </Column>
-          </Column.Group>
-          <Column.Group>
-            <Column>
-              <Field>
-                <Label>Nome</Label>
-                <Control>
-                  <Input type="text" placeholder="Text input" value={this.state.teste}/>
-                </Control>
-                <Help>Nome do grupo muscular ex. peito, costas, perna ...</Help>
-              </Field>
-            </Column>
-            <Column>
-              <Field>
-                <Label>Descricao</Label>
-                <Control>
-                  <Input type="text" placeholder="Text input" />
-                </Control>
-                <Help>Descricao para o grupo muscular</Help>
-              </Field>
-            </Column>
-          </Column.Group>
-          <Column.Group>
-            <Column>
-              <Button
-                outlined
-                color="primary"
-                onClick={this.gravarGrupoMuscular}
-              >
-                Gravar
-              </Button>
-            </Column>
-          </Column.Group>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <MenuSuperior />
+      <Container>
+        <Title>Cadastro de grupo muscular</Title>
+        <Column.Group>
+          <Column>
+            {" "}
+            <Field>
+              <Label>Nome</Label>
+              <Control>
+                <Input
+                  type="text"
+                  value={teste}
+                  onChange={e => setTeste(e.target.value)}
+                ></Input>
+              </Control>
+              <Help>Nome do grupo muscular</Help>
+            </Field>
+          </Column>
+          <Column>
+            <Field>
+              <Label>Descricao</Label>
+              <Control>
+                <Input type="text"></Input>
+              </Control>
+              <Help>Descricao do grupo muscular</Help>
+            </Field>
+          </Column>
+        </Column.Group>
+        <Column.Group>
+          <Column></Column>
+        </Column.Group>
+        <Button.Group align="right">
+          <Button color="primary" onClick={mensagem}>
+            Gravar
+          </Button>
+          <Button color="danger">Limpar</Button>
+        </Button.Group>
+      </Container>
+    </div>
+  );
 }
