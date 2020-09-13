83c7735230b0e2b04a685020734d9b34b0ea094f
Luiz Carlos <d71k1n@live.com>
2020-09-12 22:10:38 -0300

    metodo delete

--- backend/src/model/model_grupoMuscular.js
+++ backend/src/model/model_grupoMuscular.js
@@ -1,55 +1,51 @@
-const conexao = require("../database/conexao")
+const conexao = require("../database/conexao");
 class GrupoMuscular {
   //  - index
   async index(req, res) {
-    const dados = await conexao('grupoMuscular')
+    const dados = await conexao("grupoMuscular");
     res.status(200).json(dados);
   }
 
   //  - create
   async create(req, res) {
-    const {
-      nome,
-      descricao
-    } = req.body;
+    const { nome, descricao } = req.body;
 
-    const [id] = await conexao('grupoMuscular').insert({
+    const [id] = await conexao("grupoMuscular").insert({
       nome,
       descricao
-    })
+    });
     res.status(200).json(id);
   }
 
   //  -  update
   async update(req, res) {
-    const {
-      id
-    } = req.params
-    const {
-      nome,
-      descricao
-    } = req.body
+    const { id } = req.params;
+    const { nome, descricao } = req.body;
 
-    const ret = await conexao('grupoMuscular').where({
-      id
-    }).update({
-      nome,
-      descricao
-    })
+    const ret = await conexao("grupoMuscular")
+      .where({
+        id
+      })
+      .update({
+        nome,
+        descricao
+      });
     res.status(200).json(ret);
-
   }
 
   //  - select
   async select(req, res) {
-    const {
-      id
-    } = req.params
+    const { id } = req.params;
     const dados = await conexao("grupoMuscular").where({
       id
-    })
-    res.status(200).json(dados)
+    });
+    res.status(200).json(dados);
+  }
 
+  //  - delete
+  async delete(req, res) {
+    const { id } = req.params;
+    await conexao("");
   }
 }
 module.exports = GrupoMuscular;
